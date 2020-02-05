065b76b0c372b61d8ae67680f3ef386d23e0d928
Raditya Perdhevi <raditya@perdhevi.com>
2020-02-06 00:39:51 +0700

    Task: create container and utilize docker-compose

--- udacity-c3-restapi-feed/src/controllers/v0/index.router.ts
+++ udacity-c3-restapi-feed/src/controllers/v0/index.router.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 import { FeedRouter } from './feed/routes/feed.router';
 
 const router: Router = Router();
-
+console.log("feedROUTER INIT");
 router.use('/feed', FeedRouter);
 
 router.get('/', async (req: Request, res: Response) => {    
