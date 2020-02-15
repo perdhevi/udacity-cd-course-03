24b0a2448f41e9e2f08f0dc554711cb55a57d298
Raditya Perdhevi <raditya@perdhevi.com>
2020-02-15 08:20:23 +0700

    FIX: adding details steps for checkBuild.sh

--- udacity-c3-deployment/docker/checkBuild.sh
+++ udacity-c3-deployment/docker/checkBuild.sh
@@ -2,4 +2,8 @@
 echo $TRAVIS_BRANCH
 if [ "$TRAVIS_BRANCH" == "staging" ]; then
     echo "staging build"
+    docker-compose -f udacity-c3-deployment/docker/docker-compose-build-qa.yaml build --parallel
+else
+    echo "master build"
+    docker-compose -f udacity-c3-deployment/docker/docker-compose-build.yaml build --parallel
 fi
