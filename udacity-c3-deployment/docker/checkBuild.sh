fd8bb77bfe349c3881f3c5ecd8ebb892fe54ff83
Raditya Perdhevi <raditya@perdhevi.com>
2020-02-17 06:21:45 +0700

    Task:Pushing Docker images

--- udacity-c3-deployment/docker/checkBuild.sh
+++ udacity-c3-deployment/docker/checkBuild.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 echo $TRAVIS_BRANCH
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if [ "$TRAVIS_BRANCH" == "staging" ]; then
     echo "staging build"
     docker-compose -f udacity-c3-deployment/docker/docker-compose-build-qa.yaml build --parallel
+    docker images
+    docker push $DOCKER_USERNAME/udacity-restapi-user:qa
+    docker push $DOCKER_USERNAME/udacity-restapi-feed:qa
+    docker push $DOCKER_USERNAME/udacity-frontend:qa
 else
     echo "master build"
     docker-compose -f udacity-c3-deployment/docker/docker-compose-build.yaml build --parallel
+    docker images
+    docker push $DOCKER_USERNAME/udacity-restapi-user
+    docker push $DOCKER_USERNAME/udacity-restapi-feed
+    docker push $DOCKER_USERNAME/udacity-frontend:local    
 fi
