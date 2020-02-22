6ecf7f432c5622a1262e06c25885340d3ea7c14d
Raditya Perdhevi <raditya@perdhevi.com>
2020-02-22 07:02:56 +0700

    chore:adding a/b to ngingx

--- udacity-c3-deployment/k8s/destroyProd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ssh-add /Users/radityaperdhevi/OneDrive/keys/aws/demoKey.pem
-#set Environment Variable
-export AWS_ACCESS_KEY_ID=`cat ~/.aws/credentials | grep aws_access_key_id | awk '{print  $3}' `
-export AWS_SECRET_ACCESS_KEY=`cat ~/.aws/credentials | grep aws_secret_access_key | awk '{print  $3}' `
-
-kubeone reset config.yaml --tfjson .
-terraform destroy
-
